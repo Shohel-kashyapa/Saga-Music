a001a709fd0eaab70e0518770cf76c272ca19379
dilum nanayakkara <132872229+dilum77@users.noreply.github.com>
2023-06-07 12:33:42 +0530

    drawer done,account interface done

--- app/src/main/java/com/example/sagamusic/home.java
+++ app/src/main/java/com/example/sagamusic/home.java
@@ -1,14 +1,18 @@
 package com.example.sagamusic;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -20,12 +24,19 @@ public class home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         final DrawerLayout drawerLayout =findViewById(R.id.drawerLayout);
-        findViewById(R.id.navi1).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.navi2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 drawerLayout.openDrawer(GravityCompat.START);
             }
         });
+        NavigationView navigationView =findViewById(R.id.navigationView);
+        navigationView.setItemIconTintList(null);
+        NavController navController = Navigation.findNavController(this,R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(navigationView,navController);
+
+
+
 
             }
         }
