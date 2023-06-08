117695699be1334bafdf7d7e52d05e28efab04e9
Shohel-kashyapa <shohelkashapa809@gmail.com>
2023-06-09 01:11:52 +0530

    ultron commit

---
+++ app/src/main/java/com/example/sagamusic/User.java
@@ -0,0 +1,14 @@
+package com.example.sagamusic;
+
+public class User {
+    public String fullname;
+    public String email;
+
+    public User() {
+    }
+    public User(String fullname, String email) {
+        this.fullname = fullname;
+        this.email = email;
+    }
+
+}
